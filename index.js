c15374f3cbdb181dd65d3ee4759d8bd3d7937ead
Luca <luca.vaio@yahoo.fr>
2019-06-15 23:28:27 +0200

    Today I've learned – first prototype

--- index.js
+++ index.js
@@ -27,3 +27,30 @@ function openMenu() {
 		socials[0].className = "mobile-socials";
 	}
 }
+
+function post() {
+	var name = document.getElementById("name").value;
+//	var email = document.getElementById("email").value;
+	var message = document.getElementById("message").value;
+	var title = document.getElementById("title").value;
+	
+	var posts = document.getElementById("posts");
+	
+	var post = document.createElement("div");
+	var postTitle = document.createElement("h3");
+	var postMessage = document.createElement("p");
+	var postName = document.createElement("p");
+	
+	postTitle.textContent = title;
+	postMessage.textContent = message;
+	postName.textContent = name;
+	
+	post.classList.add("post");
+	postName.classList.add("author");
+	
+	post.appendChild(postTitle);
+	post.appendChild(postMessage);
+	post.appendChild(postName);
+	
+	posts.appendChild(post);
+}
